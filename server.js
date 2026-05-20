5f176d96691b1f7b8dcac02040eae5211750e30e
gserapiao <775guga@gmail.com>
2026-05-19 23:15:10 -0300

    adicionando banco

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 require("dotenv").config();
+
+const { Pool } = require("pg");
 
 const app = express();
 
@@ -10,6 +11,15 @@ const message =
     process.env.MESSAGE ||
     "Deploy funcionando!";
 
+// conexão PostgreSQL
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+// rota principal
 app.get("/", (req, res) => {
     res.send(`
         <h1>Servidor Express no Render</h1>
@@ -17,11 +27,28 @@ app.get("/", (req, res) => {
     `);
 });
 
+// rota api
 app.get("/api", (req, res) => {
     res.json({
         mensagem: "API funcionando",
         status: "online"
     });
+});
+
+// rota banco de dados
+app.get("/database", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+
+        res.json({
+            status: "Banco conectado",
+            horario: result.rows[0]
+        });
+    } catch (error) {
+        res.status(500).json({
+            erro: error.message
+        });
+    }
 });
 
 app.listen(PORT, () => {
